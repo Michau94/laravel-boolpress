ade7df438abbf10ff5d0606c405b2ef95e74c982
Michal Pawel Wadowski <80638034+Michau94@users.noreply.github.com>
2021-10-19 15:18:38 +0200

    added buttons CRUD

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -2,12 +2,14 @@
 
 @section('content')
     <div class="container">
+
         <table class="table">
             <thead>
                 <tr>
                     <th scope="col">Title</th>
                     <th scope="col">Created at:</th>
-                    <th scope="col"></th>
+                    <th scope="col" class="d-flex justify-content-end"><a href="{{ route('admin.posts.create') }}"
+                            class="btn btn-success">Create Post</a></th>
                 </tr>
             </thead>
             <tbody>
@@ -15,7 +17,17 @@
                     <tr>
                         <td>{{ $post->title }}</td>
                         <td> {{ $post->getCreationTime('created_at') }}</td>
-                        <td><a href="{{ route('admin.posts.show', $post->id) }}">Dettagli</a></td>
+                        <td class="d-flex justify-content-end">
+                            <a href="{{ route('admin.posts.show', $post->id) }}" class="btn btn-primary mr-2">Dettagli</a>
+                            <a href="{{ route('admin.posts.edit', $post->id) }}" class="btn btn-warning mr-2">Modifica</a>
+                            <form method="POST" action="{{ route('admin.posts.destroy', $post->id) }}">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-danger mr-2" type="submit">Elimina</button>
+                            </form>
+
+                        </td>
+
                     </tr>
                 @empty
                     <tr>
